c246697e46475aba166850dd0a97d31ebd0ba6be
NAKEDI ALPHABERT MALOPE <nmalope@c4r15s1.wethinkcode.co.za>
2020-06-25 12:12:14 +0200

    ex10

---
+++ ex16/r_dwssap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "sstq_, sorebrek_brk_, soibten_, sergtsop_, scodved_, rlaxcm_, rgmecived_, revreswodniw_,revressta_"
---
+++ ex18/strange_add.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FT_NBR1=\'?"\"'\
+FT_NBR2=rcrdmdd
+
+djlnljsd
